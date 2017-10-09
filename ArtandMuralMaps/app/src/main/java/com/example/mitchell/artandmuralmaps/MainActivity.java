c16e0ffbc27a5a64e19725c88cf955922238504b
HopkinsT <hopkinstaco15@gmail.com>
2017-10-09 09:46:50 -0500

    GPS

--- ArtandMuralMaps/app/src/main/java/com/example/mitchell/artandmuralmaps/MainActivity.java
+++ ArtandMuralMaps/app/src/main/java/com/example/mitchell/artandmuralmaps/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.mitchell.artandmuralmaps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
 
+public class MainActivity extends AppCompatActivity {
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button_of_power);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     //We need a map
