bf9fb1613c87de31c05aaf9cea4c5f01a7dbf968
HopkinsT <hopkinstaco15@gmail.com>
2017-10-18 09:45:33 -0500

    main
    changed from tabs

---
+++ MuralMaps/app/src/main/java/com/bas/android/muralmaps/Art.java
@@ -0,0 +1,44 @@
+package com.bas.android.muralmaps;
+
+/**
+ * Created by Mitchell on 10/15/2017.
+ */
+
+public class Art {
+    private String name;
+    private String artist;
+    private User user;
+    private int likes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+}
