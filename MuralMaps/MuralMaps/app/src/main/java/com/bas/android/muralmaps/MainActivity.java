fac4cd2994f68070ccadcd5572a0eeececd2c877
HopkinsT <hopkinstaco15@gmail.com>
2017-10-22 17:19:55 -0500

    Working Fragment switcher
    IT WORKS!

---
+++ MuralMaps/MuralMaps/app/src/main/java/com/bas/android/muralmaps/MainActivity.java
@@ -0,0 +1,139 @@
+package com.bas.android.muralmaps;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+   
+
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private FragmentTransaction mPagerAdapter;
+    private FragmentManager frag;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+    private Button toggleButton;
+    private Spinner filterSpinner;
+    private boolean state;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleButton = (Button) findViewById(R.id.toggleButton);
+        filterSpinner = (Spinner) findViewById(R.id.filterList);
+
+        frag = getSupportFragmentManager();
+        mPagerAdapter = frag.beginTransaction();
+        mPagerAdapter.add(R.id.container, new Maps_tab() , "TAG_MAP");
+        mPagerAdapter.commit();
+        toggleButton.setText("List View");
+        state = true;
+
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("Most Popular");
+        arrayList.add("Favorites");
+        arrayList.add("All");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        filterSpinner.setAdapter(adapter);
+
+        toggleButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if(state)
+                {
+                    mPagerAdapter = frag.beginTransaction();
+                    mPagerAdapter.replace(R.id.container, new TempFragment(), "TAG_List");
+                    toggleButton.setText("Map View");
+                    state = false;
+                    mPagerAdapter.commit();
+
+                }
+                else
+                {
+                    mPagerAdapter = frag.beginTransaction();
+                    mPagerAdapter.replace(R.id.container, new Maps_tab(), "TAG_List");
+                    toggleButton.setText("List View");
+                    state = true;
+                    mPagerAdapter.commit();
+
+                }
+
+            }
+        });
+
+
+      FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+      fab.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                      .setAction("Action", null).show();
+          }
+      });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+  
+
+
+
+
+}
