e20bf7d61427a7f31d4f57835960deb5b37cc762
HopkinsT <hopkinstaco15@gmail.com>
2017-10-22 16:11:32 -0500

    non working setup
    can't seem to switch using FragmentTransaction.

--- MuralMaps/app/src/main/java/com/bas/android/muralmaps/MainActivity.java
+++ MuralMaps/app/src/main/java/com/bas/android/muralmaps/MainActivity.java
@@ -1,13 +1,12 @@
 package com.bas.android.muralmaps;
 
-import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
    
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
@@ -16,8 +15,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-  
+
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,27 +33,63 @@ public class MainActivity extends AppCompatActivity {
      * may be best to switch to a
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private FragmentTransaction mPagerAdapter;
+    private FragmentManager frag;
 
     /**
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    private Button toggleButton;
+    private Spinner filterSpinner;
+    private boolean state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toggleButton = (Button) findViewById(R.id.toggleButton);
+        filterSpinner = (Spinner) findViewById(R.id.filterList);
 
+        frag = getSupportFragmentManager();
+        mPagerAdapter = frag.beginTransaction();
+        mPagerAdapter.add(R.id.container, new Maps_tab() , "TAG_MAP");
+        mPagerAdapter.commit();
+        toggleButton.setText("List View");
+        state = true;
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("Most Popular");
+        arrayList.add("Favorites");
+        arrayList.add("All");
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        filterSpinner.setAdapter(adapter);
+
+        toggleButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if(state)
+                {
+                    mPagerAdapter.replace(R.id.container, new TempFragment(), "TAG_List");
+                    toggleButton.setText("Map View");
+                    state = false;
+
+                }
+                else
+                {
+                    mPagerAdapter.replace(R.id.container, new Maps_tab(), "TAG_List");
+                    toggleButton.setText("List View");
+                    state = true;
+
+                }
+
+            }
+        });
 
 
       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -86,56 +126,10 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    
+
   
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    Maps_tab tab1 = new Maps_tab();
-                    return tab1;
-                case 1:
-                    Filter_tab tab2 = new Filter_tab();
-                    return tab2;
-
-            }
-            return null;
-
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-
-                    return "Maps";
-
-                case 1:
-                    return "Filter";
-
-            }
-            return null;
-        }
-    }
 
 
 }
