2043eb6d3ba4c9c28aeae97351f4d6e074c81310
Mitchell Fabian <mitchell.fabian@drake.edu>
2017-10-23 10:36:38 -0500

    Errored_Changes
    Accidently was working on temp. Tyler made a backup

--- MuralMaps/app/src/main/java/com/bas/android/muralmaps/MainActivity.java
+++ MuralMaps/app/src/main/java/com/bas/android/muralmaps/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bas.android.muralmaps;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,6 +19,16 @@ import android.view.View;
 import android.view.ViewGroup;
   
 import android.widget.TextView;
+import android.widget.Toast;
+
+import io.realm.ObjectServerError;
+import io.realm.Realm;
+import io.realm.SyncConfiguration;
+import io.realm.SyncCredentials;
+import io.realm.SyncUser;
+
+
+import static android.R.attr.password;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,15 +62,14 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
 
-
-      FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-      fab.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                      .setAction("Action", null).show();
-          }
-      });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
     }
 
@@ -86,8 +96,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    
-  
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -133,5 +141,53 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void login(final String email, final String password, final String username) {
+        if(SyncUser.currentUser() != null) {
+            SyncUser.currentUser().logout();
 
+            Realm realm = Realm.getDefaultInstance();
+            if(realm != null) {
+                realm.close();
+                Realm.deleteRealm(realm.getConfiguration());
+            }
+        }
+
+        SyncCredentials myCredentials = SyncCredentials.usernamePassword(email, password, false);
+        SyncUser.loginAsync(myCredentials, "http://52.205.194.154:9080", new SyncUser.Callback() {
+            @Override
+            public void onSuccess(SyncUser user) {
+                SyncConfiguration configuration = new SyncConfiguration
+                        .Builder(user, "http://52.205.194.154:9080/~/mural_maps")
+                        .disableSSLVerification().waitForInitialRemoteData().schemaVersion((long) 12.0).build();
+                Realm.setDefaultConfiguration(configuration);
+
+                Realm.getInstanceAsync(configuration, new Realm.Callback() {
+                    @Override
+                    public void onSuccess(Realm realm) {
+
+                        //successfully created a user
+                        final User user = new User();
+                        user.setUsername(username);
+                        realm.executeTransaction(new Realm.Transaction() {
+                            @Override
+                            public void execute(Realm realm) {
+                                realm.copyToRealmOrUpdate(user);
+                            }
+                        });
+
+                        realm.close();
+
+                        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                        intent.putExtra("username", username);
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @Override
+            public void onError(ObjectServerError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
